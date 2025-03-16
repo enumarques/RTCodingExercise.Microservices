307a9d6b111974030a2ad2bda82a90fa49e987cd
Eduardo Marques <201953750+enumarques@users.noreply.github.com>
2025-03-16 13:51:08 +0000

    serve plate lists and add plates in catalog service

---
+++ src/Services/Catalog/Catalog.API/Controllers/PlateController.cs
@@ -0,0 +1,61 @@
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using Catalog.API.Models;
+
+namespace Catalog.API.Controllers 
+{
+    [Route("api/[controller]")]
+    public class PlateController : Controller
+    {
+        private ILogger<PlateController> _logger {get;set;}
+        private IPlateRepository _plateRepository {get; }
+
+        public PlateController(IPlateRepository repository, ILogger<PlateController> logger)
+        {
+            _plateRepository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("list")]
+        [ProducesResponseType(typeof(PaginatedPlates), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> ListAsync(
+            [FromQuery]int pageSize = 20,
+            [FromQuery]int pageIndex = 0
+        )
+        {
+            _logger.LogInformation("Retrieving {PageSize} plates from page {PageIndex}", pageSize, pageIndex);
+            return Ok(await _plateRepository.GetPlatesAsync(pageIndex, pageSize));
+        }
+
+        [HttpPost]
+        [Route("{Id}")]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType((int)HttpStatusCode.Conflict)]
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        public async Task<IActionResult> AddPlateAsync(
+            [FromRoute]Guid Id,
+            [FromBody]Plate plateData
+        )
+        {
+            var result = await _plateRepository.AddPlateAsync(Id, plateData);
+
+            if (!result.IsSuccess)
+            {
+                if (result.Exception is ValidationException)
+                {
+                    return BadRequest(result.Exception.Message);
+                }
+                if (result.Exception is ArgumentException)
+                {
+                    return Conflict(result.Exception.Message);
+                }
+            }
+
+            return Created($"/api/plate/{Id}", result.Value);
+        }
+
+
+    }
+
+}
